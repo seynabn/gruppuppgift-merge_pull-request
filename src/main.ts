aa9e29ccf2acee989a9905269eeb88af77feabd4
rorotarver <roro9722@outlook.com>
2025-11-06 13:47:20 +0100

    added variable randomName

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import './style.css'
 
 const animalName = document.getElementById('animals') as HTMLDivElement;
 
+const randomName = document.getElementById('para') as HTMLDivElement;
+
 animalName.innerHTML = "Lion";
+
+randomName.innerHTML = "Random text";
+
