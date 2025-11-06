7e3dfbeff1bdb829eb66304bf351f1b75375a717
Seynab Nur <seynab.nur@hotmail.com>
2025-11-06 13:33:44 +0100

    added fruit list

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,34 @@
 import './style.css'
 
+class Fruit {
+  fruit:string;
+  color: string;
+
+ 
+constructor(fruit:string, color:string){
+  this.fruit=fruit;
+  this.color= color;
+}};
+
+  const fruits:Fruit[]=[
+  new Fruit("Äpple","green"),
+  new Fruit("Hallon","red"),
+  new Fruit("Apelsin","orange"),
+  new Fruit("citron","yellow")
+  ];
+
+
+
+
+
+
+  fruits.forEach(fruits)={
+    
+
+  }
+
+
+
+
 
 const fruitContainer= document.getElementById("app") as HTMLDivElement;
