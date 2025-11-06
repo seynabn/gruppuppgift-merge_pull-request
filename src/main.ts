6d53d390e2b17b2feac76c8fc8b012a3e02112aa
Seynab Nur <seynab.nur@hotmail.com>
2025-11-06 13:17:15 +0100

    börjat med lista

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,13 @@ import './style.css'
 const name = document.getElementById('animals') as HTMLDivElement;
 
 
+
+class fruit {
+  fruit:string;
+  color: string;
+}
+
+
+
+
 const fruitContainer= document.getElementById("app") as HTMLDivElement;
