b1bc9c7664ef2d7acc5779a759c89c81db62b75b
rorotarver <roro9722@outlook.com>
2025-11-06 15:03:45 +0100

    added functionlity to the variables

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,52 @@
 import './style.css'
 
+
+class Fruit{
+  name:string;
+  color: string;
+
+ 
+
+constructor(name:string, color:string){
+  this.name=name;
+  this.color= color;
+}};
+
+  const fruit:Fruit[]=[
+  new Fruit("Äpple","green"),
+  new Fruit("Hallon","red"),
+  new Fruit("Apelsin","orange"),
+  new Fruit("citron","yellow")
+  ];
+
+  
+
+
+  
+  fruit.forEach(fruit => {
+    const fruitContainer= document.getElementById("app") as HTMLDivElement;
+    const fruitName= document.createElement("h2");
+    const fruitColor=document.createElement("p");
+
+    fruitName.textContent=fruit.name;
+    fruitColor.textContent=fruit.color;
+
+    fruitContainer?.appendChild(fruitName);
+     fruitContainer?.appendChild(fruitName);
+    
+  });
+
+
+
+
+
+ 
+
+
+
+
+
+
 const animalName = document.getElementById('animals') as HTMLDivElement | null;
 const randomName = document.getElementById('para') as HTMLDivElement | null;
 
