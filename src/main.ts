876467ceb0e09885784f57a2ecb0ec21cf2fbb3f
rorotarver <roro9722@outlook.com>
2025-11-06 13:17:44 +0100

    gave the variable planetNames the value "Lion"

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import './style.css'
 
 const planetNames = document.getElementById('planets') as HTMLDivElement;
 
+planetNames.innerHTML = "Lion"
 
 const fruitContainer= document.getElementById("app") as HTMLDivElement;
