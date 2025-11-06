53c095cf37057a6b979a899ea2bd44515e50b123
rorotarver <roro9722@outlook.com>
2025-11-06 14:05:37 +0100

    created html paragraph element with a variable

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ const animalName = document.getElementById('animals') as HTMLDivElement;
 
 const randomName = document.getElementById('para') as HTMLDivElement;
 
+const randomShit = document.createElement('random') as HTMLParagraphElement;
+
+randomShit.innerHTML = "Random";
+
 animalName.innerHTML = "Lion";
 
 randomName.innerHTML = "Random text";
+
+
 
